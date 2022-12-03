80afd8e2ff85572721859eeb4e6cdb7b3b8e3bd1
unknown <muhib5532@gmail.com>
2022-12-03 19:53:45 +0600

    Gym fitness website startup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "remixicon/fonts/remixicon.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
